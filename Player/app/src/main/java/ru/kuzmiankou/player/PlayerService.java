05f74030e7dfa77436165444e87814f981750949
kuzanatoliy <njkznu@gmail.com>
2017-11-13 23:34:37 +0300

    Create project

---
+++ Player/app/src/main/java/ru/kuzmiankou/player/PlayerService.java
@@ -0,0 +1,41 @@
+package ru.kuzmiankou.player;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class PlayerService extends Service {
+
+    private MediaPlayer mPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "Service created", Toast.LENGTH_SHORT).show();
+        mPlayer = MediaPlayer.create(this, R.raw.new_year);
+        mPlayer.setLooping(false);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service started", Toast.LENGTH_SHORT).show();
+        mPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service stoped", Toast.LENGTH_SHORT).show();
+        mPlayer.stop();
+    }
+}
